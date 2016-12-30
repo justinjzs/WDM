cfc1d6e930b39793191dc6980050d9288d554798
justinjin <Justin.Jin@grapecity.com>
2016-12-30 11:13:41 +0800

    add some data

---
+++ others/create.js
@@ -0,0 +1,39 @@
+//对数据库的后端操作
+
+const pg = require('pg');
+const conString = "tcp://jin:jinzis@localhost:5432/wdm";
+
+ const client = new pg.Client(conString);
+ client.connect();
+
+//  const query = client.query( //users table
+  // `CREATE TABLE users (
+  //   U_id   int primary key not null,
+  //   U_name varchar(50) not null,
+  //   U_pwd  varchar(100) not null
+  // );`
+//  )
+
+// const query = client.query(
+//   `CREATE TABLE documents (
+//     D_hash int primary key not null,
+//     D_dir  varchar(50) not null,
+//     D_size bigint not null
+//   );`
+// )
+
+const query = client.query(
+  `CREATE TABLE u_d (
+    key     int primary key not null,
+    U_id    int not null,
+    D_hash  int ,
+    diruuid int ,
+    isdir   boolean not null,
+    path    varchar(100) not null,
+    name    varchar(50) not null,
+    createtime date not null,
+    lasttime date not null
+  );`
+)
+
+query.on('end', () => client.end() );
